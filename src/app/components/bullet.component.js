9bbe87b5739a2470dfcb4996d5cfba0588702774
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-06-05 13:40:50 -0300

    #1 Add contentEditable to bullet

--- src/app/components/bullet.component.js
+++ src/app/components/bullet.component.js
@@ -1,24 +1,42 @@
-const template = document.createElement('template');
+const CLASS = {
+  BULLET: 'bullet',
+}
 
+const KEYCODE = {
+  ENTER: 13,
+};
+
+const template = document.createElement('template');
 template.innerHTML = `
 <style>
   :host {
     display: block;
   }
 
-  :host([type="task"]):before {
+  :host([type="task"]) .bullet:before {
     content: "●";
   }
 
-  :host([type="note"]):before {
+  :host([type="note"]) .bullet:before {
     content: "-";
   }
 
-  :host([type="event"]):before {
+  :host([type="event"]) .bullet:before {
     content: "○";
   }
+
+  .bullet {
+    list-style: none;
+  }
+
+  .bullet:before {
+    display: inline-block;
+    width: 1rem;
+    margin-right: .5rem;
+    text-align: center;
+  }
 </style>
-<slot></slot>
+<li class="${CLASS.BULLET}"></li>
 `
 
 export class Bullet extends HTMLElement {
@@ -26,5 +44,40 @@ export class Bullet extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this._setLabel();
+    this._setEvents();
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector(`.${CLASS.BULLET}`)
+      .removeEventListener('click', this._onBulletClick);
+    this.shadowRoot.querySelector(`.${CLASS.BULLET}`)
+      .removeEventListener('keydown', this._onBulletKeydown);
+  }
+
+  _onBulletClick(event) {
+    event.target.setAttribute('contentEditable', true);
+  }
+
+  _onBulletKeydown(event) {
+    if (event.keyCode === KEYCODE.ENTER) {
+      event.preventDefault();
+      event.target.blur();
+    }
+  }
+
+  _setEvents() {
+    this.shadowRoot.querySelector(`.${CLASS.BULLET}`)
+      .addEventListener('click', this._onBulletClick);
+    this.shadowRoot.querySelector(`.${CLASS.BULLET}`)
+      .addEventListener('keydown', this._onBulletKeydown);
+  }
+
+  _setLabel() {
+    const bullet = this.shadowRoot.querySelector(`.${CLASS.BULLET}`);
+    bullet.innerHTML = this.getAttribute('label');
   }
 }
