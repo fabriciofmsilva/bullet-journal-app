488824d3bc6cc65d664e4cecb2d184a64a298925
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-05 02:06:21 +0000

    Deploy fabriciofmsilva/bullet-journal-app to github.com/fabriciofmsilva/bullet-journal-app.git:gh-pages

---
+++ precache-manifest.f49f6a062d3b15d5e922c3c435ed54ae.js
@@ -0,0 +1,24 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "icon_128x128.00b047cc8e22d931b3f430a606ab8295.png"
+  },
+  {
+    "url": "icon_192x192.cf3fa51cb48b60dfc494521b8be80e91.png"
+  },
+  {
+    "url": "icon_256x256.dd9d1a0ba319144e03565719c629e766.png"
+  },
+  {
+    "url": "icon_512x512.5ee80270347eb2c694399874384c524c.png"
+  },
+  {
+    "revision": "97ec49a7a0f662193bbdb4815da9b9ff",
+    "url": "index.html"
+  },
+  {
+    "url": "main.eb51de1d751a1206e4f8.js"
+  },
+  {
+    "url": "manifest.e44df5a3efe394c0f088b6ce8ffc31b3.json"
+  }
+]);
